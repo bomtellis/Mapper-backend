9169a771a2881655f64c8228790678c327f893e1
bomtellis <52172722+bomtellis@users.noreply.github.com>
2019-06-27 23:00:13 +0100

    Auto zoom 5

--- app/handlers/convertPdf.js
+++ app/handlers/convertPdf.js
@@ -242,7 +242,7 @@ function tileup(pngFilePath, outputDir) {
         folders.then(function()
         {
             // dir exists
-            let command = "tileup --auto-zoom 4 --prefix map_tile --in " + pngFilePath + " --output-dir " + outputDir;
+            let command = "tileup --auto-zoom 5 --prefix map_tile --in " + pngFilePath + " --output-dir " + outputDir;
             exec(command, (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr);
@@ -258,20 +258,22 @@ function tileup(pngFilePath, outputDir) {
 function renameFolders(mapPath)
 {
     return new Promise(function(resolve, reject) {
-        fs.rename(mapPath + "/17", mapPath + "/0", (err) => {
-            if(err) reject(err);
-            fs.rename(mapPath + "/18", mapPath + "/1", (err) => {
+        fs.rename(mapPath + "/16", mapPath + "/0", (err) => {
+            fs.rename(mapPath + "/17", mapPath + "/1", (err) => {
                 if(err) reject(err);
-                fs.rename(mapPath + "/19", mapPath + "/2", (err) => {
+                fs.rename(mapPath + "/18", mapPath + "/2", (err) => {
                     if(err) reject(err);
-                    fs.rename(mapPath + "/20", mapPath + "/3", (err) => {
+                    fs.rename(mapPath + "/19", mapPath + "/3", (err) => {
                         if(err) reject(err);
-                        console.log("Renamed folders");
-                        resolve("true");
+                        fs.rename(mapPath + "/20", mapPath + "/4", (err) => {
+                            if(err) reject(err);
+                            console.log("Renamed folders");
+                            resolve("true");
+                        });
                     });
                 });
             });
-        });
+        })
     });
 }
 
